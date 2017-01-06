cccbde25d3ca3b33a32e0892754f440bf689486c
controlzone <controlzone@users.noreply.github.com>
2017-01-06 22:38:54 +1100

    Working my way through

--- cars.py
+++ cars.py
@@ -16,6 +16,9 @@ IMAGE_DIR = "images"
 IMAGE_FILENAME_FORMAT = IMAGE_DIR + "/frame_%04d.png"
 KEYPOINT_DATA_FILE = "keypoints.p"
 
+##    Keypoints looks likeit beeds to have the SIFT func
+##    put over the cars.png image and stored in the pickle .p
+
 # Support either video file or individual frames
 CAPTURE_FROM_VIDEO = False
 if CAPTURE_FROM_VIDEO:
@@ -89,21 +92,29 @@ def process_frame(frame_number, frame, keypoint_data, detector, matcher):
 # ============================================================================
 
 def main():
+    print "start Main()"
     log = logging.getLogger("main")
     
     log.debug("Loading keypoint data from '%s'...", KEYPOINT_DATA_FILE)
-    keypoint_data = KeypointData.load(KEYPOINT_DATA_FILE)
-    
+    try:
+        keypoint_data = KeypointData.load(KEYPOINT_DATA_FILE)
+        print "Loaded keypoints"
+    except:
+        print "failed to load keypoints"
+        
     log.debug("Creating SIFT detector...")
-    sift = cv2.SIFT(nfeatures=0, nOctaveLayers=5, contrastThreshold=0.05, edgeThreshold=30, sigma=1.5)
+#    sift = cv2.SIFT(nfeatures=0, nOctaveLayers=5, contrastThreshold=0.05, edgeThreshold=30, sigma=1.5)
+    sift = cv2.xfeatures2d.SIFT_create(nfeatures=0, nOctaveLayers=5, contrastThreshold=0.05, edgeThreshold=30, sigma=1.5)
     bf = cv2.BFMatcher()
 
     # Set up image source
     log.debug("Initializing video capture device #%s...", IMAGE_SOURCE)
     cap = cv2.VideoCapture(IMAGE_SOURCE)
     
-    frame_width = cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
-    frame_height = cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+##    frame_width = cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)     #
+##    frame_height = cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)   #
+    frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)       # OpenCV 3.0.0
+    frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)     # OpenCV 3.0.0
     log.debug("Video capture frame size=(w=%d, h=%d)", frame_width, frame_height)
 
     log.debug("Starting capture loop...")
@@ -149,10 +160,10 @@ def main():
 
 if __name__ == "__main__":
     log = init_logging()
-    
+    print "log init"
     if not os.path.exists(IMAGE_DIR):
         log.debug("Creating image directory `%s`...", IMAGE_DIR)
         os.makedirs(IMAGE_DIR)
-    
+        "Image dir created"
     main()
     
